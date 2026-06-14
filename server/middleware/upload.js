60aa841a8da8e6354b9023fc02eb54b71f6e1f57
KeLar36 <deni.kelar@gmail.com>
2026-06-14 20:55:37 +0300

    fix: fixed upload middleware

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -2,6 +2,7 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 
+// Ініціалізація Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
@@ -10,21 +11,23 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
-  params: async (req, file) => {
+  params: (req, file) => {
     const isImage = file.mimetype.startsWith("image/");
+    const cleanFileName = file.originalname
+      .split(".")[0]
+      .replace(/[^a-zA-Z0-9]/g, "_");
 
     return {
       folder: isImage ? "science_hub/covers" : "science_hub/articles",
       resource_type: "auto",
-      public_id: `${Date.now()}-${file.originalname.split(".")[0]}`,
-      access_mode: "public",
+      public_id: `${Date.now()}-${cleanFileName}`,
     };
   },
 });
 
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 10 * 1024 * 1024 },
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10 MB
 });
 
 module.exports = upload;
