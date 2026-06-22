851f976747132451a0eb1c3b22ef6510ace8f960
KeLar36 <deni.kelar@gmail.com>
2026-06-22 14:42:19 +0300

    fix: fix vercel config for proper mobile auth functionality

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -77,7 +77,7 @@ router.post("/login", async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "none",
+      sameSite: "lax",
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 днів
     });
 
