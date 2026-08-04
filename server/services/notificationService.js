3c7b3fd49b448b782de5efa5e61a7787ba222bf8
KeLar36 <deni.kelar@gmail.com>
2026-08-04 22:35:29 +0300

    Fix: teresva vs tereblya

--- server/services/notificationService.js
+++ server/services/notificationService.js
@@ -1,4 +1,4 @@
-const Notification = require("../models/Notification");
+const Notification = require("../models/notification");
 const User = require("../models/User");
 const nodemailer = require("nodemailer");
 
